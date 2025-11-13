90f677abc91d8c3a5d2b55d5515babe236f54cc8
holberton <12495@holbertonstudents.com>
2025-11-13 12:41:22 +0000

    .

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:66], str[-22:-18], str[:6])
-#
+print(str[41:63] + str[64:68] + str[69:78] + str[79:81] + str[82:88] + str[88:90] + str[91:97] + str[97:103] + str[103:104] + str[105:111] + str[111:112] + str[113:119] + str[119:120])
