37829edd4a38fd031836af7e2aa377e9698381e9
holberton-student <12495@hoblertonstudents.com>
2025-11-15 11:48:56 +0000

    .

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = str(number)[-1]
+    last_digit = number % 10 if number >= 0 else -number % 10
     print(last_digit.format(), end='')
     return last_digit
