7cc6533127e2ec89906a89fc237ec12344ba05b2
holberton-studnet <12495@hoblertonstudnets.com>
2025-11-17 10:31:54 +0000

    autocommit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-if __name__="__main__":
-    print(__import__("add_0").add(3, 4))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, __import__("add_0").add(a, b)))
