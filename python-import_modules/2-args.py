45f76300f39fd8c8f5a6eb5e8df1cb8c64a254b9
holberton-studnet <12495@hoblertonstudnets.com>
2025-11-17 11:13:48 +0000

    autocommit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 if __name__ == "__main__":
     import sys
     arg_len = len(sys.argv) - 1
+    all_args = [f"{i+1}: {arg}" for i, arg in enumerate(sys.argv[1:])]
     if arg_len == 0:
         print(f"{arg_len} arguments.")
+    elif arg_len == 1:
+        print(f"{arg_len} argument:")
+        print(all_args[0])
     else:
         print(f"{arg_len} arguments:")
-        for i, arg in enumerate(sys.argv[1:]):
-            print(f"{i+1}: {arg}")
+        print("\n".join(all_args))
