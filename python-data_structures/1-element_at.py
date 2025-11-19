aa072346a120c5b446c986896fa9e6343d4096d9
holberton-student <12495@holbertonstudents.com>
2025-11-19 07:19:07 +0000

    autocommit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-def element_at(lst, index):
-    if index < 0:
-        # negative index is valid only if within -len(lst) to -1
-        if index >= -len(lst):
-            return lst[index]
-        else:
-            return None
-    else:
-        # non-negative index must be < len(lst)
-        if index < len(lst):
-            return lst[index]
-        else:
-            return None
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
