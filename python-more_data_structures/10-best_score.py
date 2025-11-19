aca06b154e19ba267919d524bae44ca3b8cc13f3
holberton-student <12495@holbertonstudents.com>
2025-11-19 12:44:53 +0000

    autocommit

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        max_score = max(i for i in a_dictionary.values())
+        inverted_dict = {val: key for key, val in a_dictionary.items()}
+        return inverted_dict[max_score]
