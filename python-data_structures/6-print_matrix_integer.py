0893ae79d09b76bf0c2dd8f4156219fda55a9f2c
holberton-student <12495@holbertonstudents.com>
2025-11-19 07:49:05 +0000

    autocommit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix):
     for row in matrix:
         for col in row:
             if row[-1] == col:
-                print("{}".format(col))
+                print("{:d}".format(col))
             else:
-                print("{}".format(col), end=' ')
+                print("{:d}".format(col), end=' ')
 
