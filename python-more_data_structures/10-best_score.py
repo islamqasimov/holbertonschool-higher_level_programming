bddee745442b95cb8888457edb6e3969983b6f94
holberton-student <12495@holbertonstudents.com>
2025-11-19 12:48:43 +0000

    autocommit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,5 +4,7 @@ def best_score(a_dictionary):
         max_score = max(i for i in a_dictionary.values())
         inverted_dict = {val: key for key, val in a_dictionary.items()}
         return inverted_dict[max_score]
-    except:
+    except UnboundLocalError:
+        return None
+    except AttributeError:
         return None
