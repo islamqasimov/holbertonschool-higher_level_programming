52ae421907248d5c1471545562b7eaf1041f95e1
islamqasimov <12495@holbertonstudents.com>
2025-11-21 17:02:53 +0000

    autocommit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo apt install micro
+pip3 install pycodestyle
+git config --global user.email "12495@holbertonstudents.com"
+git config --global user.name "islamqasimov"
+alias gp="git add .; git commit -m 'autocommit'; git push"
