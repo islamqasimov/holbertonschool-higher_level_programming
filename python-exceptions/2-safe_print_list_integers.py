a5c1ad12fd96fb6e44854398ba5fe0f23f08a41c
islamqasimov <12495@holbertonstudents.com>
2025-11-22 12:43:13 +0000

    autocommit

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             el = my_list[i]
             if type(el) == type(5):
-                print(el, end='')
+                print("{:d}".format(el), end='')
                 counter += 1
         print()
     except (ValueError, TypeError, IndexError):
