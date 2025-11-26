b5e6d9a96a0e004636fa0cc5434b91bf3c222629
islamqasimov <12495@holbertonstudents.com>
2025-11-26 12:08:20 +0000

    autocommit

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-The lookup() function simply calls dir() on the object and returns the result as a list.
+The lookup() function simply calls dir() on the object
+and returns the result as a list.
 """
 
 
 def lookup(obj):
     """
-    The lookup() function simply calls dir() on the object and returns the result as a list.
+    The lookup() function simply calls dir() on the object and
+    returns the result as a list.
     """
     return dir(obj)
