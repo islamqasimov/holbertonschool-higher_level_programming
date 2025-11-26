676f907c39940b14d94b845cb1a0e30de2a9b946
islamqasimov <12495@holbertonstudents.com>
2025-11-26 12:30:05 +0000

    autocommit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-
+List module inheritance
 """
 
 
 class MyList(list):
+    """
+    List child class, with extra print sorted method
+    """
     def print_sorted(self):
         print(sorted(self))
