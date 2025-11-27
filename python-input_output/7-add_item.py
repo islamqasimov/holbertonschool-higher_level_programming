1c27b1800b62e8f2a9effc37a9966ea0b4838644
islamqasimov <12495@holbertonstudents.com>
2025-11-27 12:35:10 +0000

    autocommit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,12 @@ Testing script that adds arguments to json file
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_f = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_f = __import__('6-load_from_json_file').load_from_json_file
     filename = 'add_item.json'
     args = sys.argv[1:]
     try:
-        content = load_from_json_file(filename)
-        save_to_json_file(content + args, filename)
+        content = load_from_json_f(filename)
+        save_to_json_f(content + args, filename)
     except FileNotFoundError:
-        save_to_json_file(args, filename)
+        save_to_json_f(args, filename)
