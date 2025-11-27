7a0dbe42f4b4ebe5c2fe30ce1970ade408d38fb7
islamqasimov <12495@holbertonstudents.com>
2025-11-27 10:28:35 +0000

    autocommit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
