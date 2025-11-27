bec1ab3d27ca44b39cd8023562e90d7837852303
islamqasimov <12495@holbertonstudents.com>
2025-11-27 11:28:39 +0000

    autocommit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module to read and print out a file
+"""
+
+
 def read_file(filename=""):
     with open(filename, 'r', encoding='UTF-8') as file:
         print(file.read())
