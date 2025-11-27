8cb5d14d581292565a3b70e8be85e764a67251e2
islamqasimov <12495@holbertonstudents.com>
2025-11-27 10:41:09 +0000

    autocommit

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError(f"{name} must be greater than 0")
+        else:
+            raise TypeError(f"{name} must be an integer")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Subclass of BaseGeometry that represents Rectangle.
+    """
+    def __init__(self, width, heigth):
+        self.__width = self.integer_validator("width", width)
+        self.__heigth = self.integer_validator("heigth", heigth)
