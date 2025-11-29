d25e3c0d1e82cd60c843559a9d39a5fd50963946
islamqasimov <12495@holbertonstudents.com>
2025-11-29 21:38:11 +0000

    autocommit

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for defining Student
+"""
+
+
+class Student:
+    """
+    Class that represents student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dct = self.__dict__
+        if attrs is not None:
+            output = {}
+            for at in attrs:
+                if at in dct.keys():
+                    output[at] = dct[at]
+            return output
+        return dct
