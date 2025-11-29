d38f7ed19b1dfb3005143ea48f9260f3cbffdb5c
islamqasimov <12495@holbertonstudents.com>
2025-11-29 22:15:19 +0000

    autocommit

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        current_row = [1]
+        prev_row = triangle[i-1]
+        for j in range(len(prev_row) - 1):
+            current_row.append(prev_row[j] + prev_row[j+1])
+        current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
