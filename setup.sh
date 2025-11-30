02936fd2e0aa9f1d86e2e03078842ed06c20fac2
islamqasimov <12495@holbertonstudents.com>
2025-11-30 15:04:38 +0000

    autocommit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-sudo apt install micro
+apt update
+apt install mysql-server micro -y
 pip3 install pycodestyle
+service mysql start
 git config --global user.email "12495@holbertonstudents.com"
 git config --global user.name "islamqasimov"
 alias gp="git add .; git commit -m 'autocommit'; git push"
+alias mysqlr='mysql -hlocalhost -uroot -p'
