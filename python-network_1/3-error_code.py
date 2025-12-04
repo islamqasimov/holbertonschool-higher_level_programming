88aaae28ff339a4dd6e03897875a2c34b4f5a474
islamqasimov <12495@holbertonstudents.com>
2025-12-04 12:15:47 +0000

    autocommit

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Script that displays body of the error code
+"""
+
+
+import urllib.error
+import urllib.request
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+
+if __name__ == '__main__':
+    main()
