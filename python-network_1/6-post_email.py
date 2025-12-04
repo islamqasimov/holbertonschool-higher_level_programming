d2cbf9b1a95903174b9ddea02881a3b168b86cb0
islamqasimov <12495@holbertonstudents.com>
2025-12-04 21:41:07 +0000

    autocommit

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays the body of the response
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    rsp = requests.post(url, data=payload)
+
+    print(rsp.text)
+
+
+if __name__ == '__main__':
+    main()
