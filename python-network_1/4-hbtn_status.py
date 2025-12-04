dac4764df3c2767380c6719030050a5de5c49548
islamqasimov <12495@holbertonstudents.com>
2025-12-04 21:28:31 +0000

    autocommit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ Python script that fetches https://intranet.hbtn.io/status
 
 import requests
 
+
 def main():
     url = 'https://intranet.hbtn.io/status'
     rsp = requests.get(url)
@@ -13,7 +14,6 @@ def main():
     print("Body response:")
     print("\t- type:", type(rsp.text))
     print("\t- content:", rsp.text)
-    
 
 
 if __name__ == '__main__':
