03e56d52dc5f48bda3a0082214866b053e5427eb
islamqasimov <12495@holbertonstudents.com>
2025-12-04 21:32:23 +0000

    autocommit

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the 
+value of the variable X-Request-Id in the response header
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    rsp = requests.get(url)
+
+    print(rsp.headers.get("X-Request-Id"))
+
+
+if __name__ == '__main__':
+    main()
