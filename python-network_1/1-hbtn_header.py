9ff486b79c28113b5c555a0f96207ee5a25d7c94
islamqasimov <12495@holbertonstudents.com>
2025-12-04 10:10:11 +0000

    autocommit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Script to get X-Request-Id"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        data = response.info()
+        print(data.get('X-Request-Id'))
