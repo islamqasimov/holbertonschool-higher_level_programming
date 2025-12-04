84c3ada1bb1c11d74a6767c9b6b6ca30a0b3d973
islamqasimov <12495@holbertonstudents.com>
2025-12-04 10:36:03 +0000

    autocommit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Script that sends POST request with email header
+"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    request = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
+        print(body)
+
+if __name__ == '__main__':
+    main()
