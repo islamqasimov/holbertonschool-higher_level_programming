f2458195d99ff89daec2ff276b57e160c84eb66e
islamqasimov <12495@holbertonstudents.com>
2025-12-04 21:46:04 +0000

    autocommit

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL, sends
+a request to the URL and displays the body of the response
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    
+    try:
+        rsp = requests.get(url)
+        rsp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print("Error code:", rsp.status_code)
+
+    
+
+
+if __name__ == '__main__':
+    main()
