b11a8a742f040e4cd61ccc4adedd1964f81437f8
islamqasimov <12495@holbertonstudents.com>
2025-12-05 18:27:09 +0000

    autocommit

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -13,18 +13,35 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
     Custom API Handler
     """
 
-    def do_GET(self):
+    def _send_headers(self, status_code=200, content_type='text/plain'):
         # Sending HTTP response status code
-        self.send_response(200)
+        self.send_response(status_code)
 
         # Sending Headers
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", content_type)
         self.end_headers()
 
-        # Writing response body
-        self.wfile.write(b"Hello, this is a simple API!")
+    def do_GET(self):
+        if self.path == '/':
+            self._send_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+            return
 
-        return
+        elif self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data).encode()
+
+            self._send_headers(200, 'application/json')
+            self.wfile.write(json_data)
+            return
+
+        elif self.path == '/status':
+            self._send_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self._send_headers(404)
+            self.wfile.write(b"What u trin to test gigga ???")
 
 
 def run(server_class=http.server.HTTPServer, handler_class=SimpleAPIHandler):
